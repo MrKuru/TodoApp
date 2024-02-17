1522c63fbf8faae24680a9302265c50649c954d9
MrKuru <melihcan@getluvu.com>
2024-02-17 19:23:12 +0300

    ApplicationProperties edited

--- src/main/java/com/hb/configuration/CouchbaseConfiguration.java
+++ src/main/java/com/hb/configuration/CouchbaseConfiguration.java
@@ -1,5 +1,6 @@
 package com.hb.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
@@ -8,22 +9,35 @@ import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepos
 @EnableCouchbaseRepositories(basePackages = "com.hb.dao.repositories")
 public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
 
+    @Value("${spring.couchbase.bootstrap-hosts}")
+    private String bootstrapHosts;
+
+    @Value("${spring.couchbase.username}")
+    private String username;
+
+    @Value("${spring.couchbase.password}")
+    private String password;
+
+    @Value("${spring.couchbase.bucket.name}")
+    private String bucketName;
+
     @Override
     public String getConnectionString() {
-        return "couchbase://127.0.0.1";
+        return bootstrapHosts;
     }
 
     @Override
     public String getUserName() {
-        return "admin";
+        return username;
     }
 
     @Override
     public String getPassword() {
-        return "password";
+        return password;
     }
+
     @Override
     public String getBucketName() {
-        return "hb";
+        return bucketName;
     }
 }
